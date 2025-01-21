920d009e78143e1fd6643455c33a91c06577fd4c
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2025-01-21 19:01:35 +0100

    server routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
+const userRoutes = require("./routes/users.routes");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use("/", userRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log("Server is running on port" + port);
 });
